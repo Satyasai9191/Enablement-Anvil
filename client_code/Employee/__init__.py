81dd903fe329d6317f704ba1cc7bac566e337b28
gowdanarsanjana@gmail.com <gowdanarsanjana@gmail.com>
2024-07-05 04:36:04 +0000

    Created Admin, Admin.ManageContent, Admin.ManageUsers, Admin.ManageUsers.Form1 and 1 more; Edited settings; Deleted Form1

---
+++ client_code/Employee/__init__.py
@@ -0,0 +1,10 @@
+from ._anvil_designer import EmployeeTemplate
+from anvil import *
+
+
+class Employee(EmployeeTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
